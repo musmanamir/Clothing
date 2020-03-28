039c0a25cff32b158c3b97d781babbf8e882341f
Usman Amir <usman.amir@aciano.net>
2020-03-28 21:52:13 +0500

    initial push

---
+++ Clothing_/Connection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Clothing_
+{
+    public class Connection
+    {
+        public static SqlConnection sc;
+        public static string dbCon = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+        public static SqlConnection get()
+        {
+            if (sc == null)
+            {
+                sc = new SqlConnection();
+                sc.ConnectionString = dbCon;
+                sc.Open();
+            }
+            return sc;
+        }
+    }
+}
